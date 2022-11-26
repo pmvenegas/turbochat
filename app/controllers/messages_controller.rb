cb629ee296c9a737471e324506aff4d04a451a1f
pmv <pvenegas@gmail.com>
2022-11-26 22:39:23 +0800

    Use Turbo frames for messaging UI

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,40 +1,39 @@
 class MessagesController < ApplicationController
   before_action :set_message, only: %i[ show edit update destroy ]
 
-  # GET /messages or /messages.json
   def index
     @messages = Message.all
+    @message = Message.new
   end
 
-  # GET /messages/1 or /messages/1.json
   def show
   end
 
-  # GET /messages/new
   def new
     @message = Message.new
   end
 
-  # GET /messages/1/edit
   def edit
   end
 
-  # POST /messages or /messages.json
   def create
     @message = Message.new(message_params)
 
     respond_to do |format|
       if @message.save
-        format.html { redirect_to message_url(@message), notice: "Message was successfully created." }
+        format.html { redirect_to action: 'index' }
         format.json { render :show, status: :created, location: @message }
       else
-        format.html { render :new, status: :unprocessable_entity }
+        format.turbo_stream do
+          render turbo_stream: turbo_stream.replace(@message,
+                                                    partial: 'messages/form',
+                                                    locals: { message: @message })
+        end
         format.json { render json: @message.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # PATCH/PUT /messages/1 or /messages/1.json
   def update
     respond_to do |format|
       if @message.update(message_params)
@@ -47,7 +46,6 @@ class MessagesController < ApplicationController
     end
   end
 
-  # DELETE /messages/1 or /messages/1.json
   def destroy
     @message.destroy
 
@@ -58,12 +56,10 @@ class MessagesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_message
       @message = Message.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def message_params
       params.require(:message).permit(:body)
     end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,6 @@
 class Message < ApplicationRecord
   validates :body, presence: true
+
+  # TODO: implement channels
+  broadcasts_to ->(message) { "messages" }
 end
