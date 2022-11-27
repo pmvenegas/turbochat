f109704bc371f4ad7e29549ea995c84afdda2feb
pmv <pvenegas@gmail.com>
2022-11-27 17:39:07 +0800

    Clean up JS semicolons

--- app/javascript/controllers/message_controller.js
+++ app/javascript/controllers/message_controller.js
@@ -4,12 +4,12 @@ export default class extends Controller {
   static targets = ["info", "controls"]
 
   show_controls() {
-    this.infoTarget.classList.add("d-none");
-    this.controlsTarget.classList.remove("d-none");
+    this.infoTarget.classList.add("d-none")
+    this.controlsTarget.classList.remove("d-none")
   }
 
   hide_controls() {
-    this.infoTarget.classList.remove("d-none");
-    this.controlsTarget.classList.add("d-none");
+    this.infoTarget.classList.remove("d-none")
+    this.controlsTarget.classList.add("d-none")
   }
 }
